05efdee1bd68077d24a90c2e9437907252144dab
a-elrawy <abdelrhman.elrawy1@gmail.com>
2022-06-29 00:33:34 +0200

    refactoring

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ getElectronsNumber = {
     'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 'Fm': 100,
     'Md': 101, 'No': 102, 'Lr': 103
 }
+getMassNumber = {
+    'H': 1, 'He': 4, 'Li': 7, 'Be': 9, 'B': 11, 'C': 12, 'N': 14, 'O': 16, 'F': 19, 'Ne': 20, 'Na': 23, 'Mg': 24, 'Al': 27, 'Si': 28, 'P': 31, 'S': 32, 'Cl': 35, 'Ar': 40, 'K': 39, 'Ca': 40, 'Sc': 45, 'Ti': 48, 'V': 51, 'Cr': 52, 'Mn': 55, 'Fe': 56, 'Co': 59, 'Ni': 58, 'Cu': 63, 'Zn': 65, 'Ga': 70, 'Ge': 73, 'As': 75, 'Se': 79,
+    'Br': 80, 'Kr': 84, 'Rb': 85, 'Sr': 88, 'Y': 89, 'Zr': 91, 'Nb': 92, 'Mo': 95, 'Tc': 98, 'Ru': 101, 'Rh': 102, 'Pd': 106, 'Ag': 107, 'Cd': 114, 'In': 115, 'Sn': 120, 'Sb': 121, 'Te': 128, 'I': 127, 'Xe': 132, 'Cs': 133, 'Ba': 138, 'La': 139, 'Ce': 140, 'Pr': 141, 'Nd': 142, 'Pm': 145, 'Sm': 150, 'Eu': 151, 'Gd': 158, 'Tb': 159,
+    'Dy': 162, 'Ho': 165, 'Er': 166, 'Tm': 169, 'Yb': 174, 'Lu': 175, 'Hf': 180, 'Ta': 181, 'W': 184, 'Re': 187, 'Os': 192, 'Ir': 193, 'Pt': 195, 'Au': 197, 'Hg': 202, 'Tl': 205, 'Pb': 208, 'Bi': 209, 'Po': 210, 'At': 210, 'Rn': 211, 'Fr': 223, 'Ra': 226, 'Ac': 227, 'Th': 232, 'Pa': 231, 'U': 238, 'Np': 237, 'Pu': 244, 'Am': 243, 'Cm': 247,
+    'Bk': 247, 'Cf': 251, 'Es': 252, 'Fm': 257, 'Md': 258, 'No': 259, 'Lr': 262
+};
 electronPositions = {
     0 : ['-1.99 0 0','1.99 0 0'],
     1 : ['-2.99 0 0','2.99 0 0','0 2.99 0','0 -2.99 0','-2.22 -1.94 0','-2.22 1.94 0','2.22 -1.94 0','2.22 1.94 0'],
@@ -47,7 +53,9 @@ const getAtom = ((model) => {
     text.setAttribute('text-geometry',"value: "+ model + "; bevelSize: 7.77; bevelThickness: 12.46; curveSegments: 12.76; height: 0.08; size: 0.6; weight: bold");
     text.setAttribute('material',"");
     const atom = document.createElement('a-sphere');
-    atom.setAttribute('radius', 1.25);
+    let radius = 1 + getMassNumber[model]/100;
+    atom.setAttribute('radius', radius);
+    atom.setAttribute('class', 'atom');
     atom.setAttribute('color', "#1913cf");
     atom.appendChild(text);
     return atom;
@@ -104,7 +112,6 @@ const getAnnotation= (() => {
     triangle.setAttribute('material', "color: #f6d32d");
     triangle.setAttribute('scale', "0.39 0.26 4.21");
     triangle.setAttribute('rotation', "0 90 180");
-    console.log(triangle);
 
     const text = document.createElement('a-entity');
     text.setAttribute('text-geometry',"value: Electron (e -ve ); bevelSize: 7.77; bevelThickness: 12.46; curveSegments: 12.76; height: 0.08; size: 0.4; weight: bold");
@@ -116,4 +123,28 @@ const getAnnotation= (() => {
     annotation.appendChild(arrow);
     annotation.appendChild(triangle);
     return annotation;
+});
+
+const getMixtureModel = ((model1,model2) => {
+    const m1= generateEntity(model1)
+    const m2= generateEntity(model2)
+    m1.setAttribute('position', '4 0 0');
+    m2.setAttribute('position', '-4 0 0');
+    m1.setAttribute('animation', "property: position; to: 1 0 0;; dur: 2000; easing: linear")
+    m2.setAttribute('animation', "property: position; to: -1 0 0;; dur: 2000; easing: linear")
+    // Adjust based on Mass number
+    m1.querySelector('.atom').setAttribute('animation', "property: position; to:-2 0 0; dur: 6000; easing: easeInExpo")
+    m2.querySelector('.atom').setAttribute('animation', "property: position; to:4.2 0 0; dur: 6000; easing: easeInExpo")
+    m1.querySelectorAll('a-torus').forEach(function(e){
+        e.setAttribute('animation', "property: rotation; to: 90 0 180; loop: true; dur: 5000; easing: linear")
+        e.setAttribute('animation__2', "property: visible; from: true; to: false; dur: 9999999999999999991")
+    })
+    m2.querySelectorAll('a-torus').forEach(function(e){
+        e.setAttribute('animation', "property: rotation; to: 90 0 180; loop: true; dur: 5000; easing: linear")
+        e.setAttribute('animation__2', "property: visible; from: true; to: false; dur: 9999999999999999991")
+    })
+    const model= document.createElement('a-entity')
+    model.appendChild(m1)
+    model.appendChild(m2)
+    return model;
 });
